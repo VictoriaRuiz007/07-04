854408ac26c2b1acd55ab1680e3ccb88221f9a61
VictoriaRuiz007 <vict.ruiza@duocuc.cl>
2025-04-07 11:08:01 -0400

    Add files via upload

---
+++ clase1.py
@@ -0,0 +1,22 @@
+"""
+print(" - - -")
+print("| hola |")
+print(" - - -")
+
+
+variable01 = 12
+variable02 = 12,3
+variabletext = "Hola"
+variablelogic = True
+"""
+"""
+nombre =  "pepe"
+print("       - - - ")
+print("Hola  |", nombre, "|")
+print("       - - - ")
+"""
+
+nombre = input ("Dime tu nombre: ")
+apellido = input("Dime tu apellido: ")
+edad = input("Dime tu edad: ")
+print("Te llamas ", nombre, apellido, "y tienes ", edad, "años")
